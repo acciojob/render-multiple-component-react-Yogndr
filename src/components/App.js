29ee5b4cc6c1a20b343f3bf97c51aff36ada629e
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-07-18 12:29:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-import card from ./components/card
+import Card from ./components/Card
 
 const App = () => {
   return (
     <div id="main">
-    <card name="ABC" desc="My ame is knmjs"  />
-    <card name="DEF" desc="bsn hjwbd qwnd"  />
+    <Card name="ABC" desc="My ame is knmjs"  />
+    <Card name="DEF" desc="bsn hjwbd qwnd"  />
     </div>
   )
 }
