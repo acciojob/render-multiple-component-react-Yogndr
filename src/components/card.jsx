9d62212ce923b51fb434332279fb15c8c2f2033d
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-07-18 12:20:54 +0530

    Create card.jsx

---
+++ src/components/card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const card=(props)=>{
+ return( 
+<div class="ns-wrapper">
+<h1 data-ns-test="project-name">props.name</h1>
+<h6 data-ns-test="project-description">props.desc</h6>
+</div>
+   )
+
+}
+
+
