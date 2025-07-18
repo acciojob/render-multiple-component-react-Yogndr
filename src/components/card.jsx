60476e9fe9e819d130c3730b2552b6f531143a67
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-07-18 12:26:01 +0530

    Update card.jsx

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 const card=(props)=>{
  return( 
 <div class="ns-wrapper">
-<h1 data-ns-test="project-name">props.name</h1>
-<h6 data-ns-test="project-description">props.desc</h6>
+<h1 data-ns-test="project-name">{props.name}</h1>
+<h6 data-ns-test="project-description">{props.desc}</h6>
 </div>
    )
 
 }
+export default card;
 
 
